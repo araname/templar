944c5e54135a721f32fe46ecede29fbaf6689800
Myeongjin <aranet100@gmail.com>
2016-04-20 00:45:17 +0900

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Korean language file for template
+ *
+ */
+
+$lang['discussion']        = '토론';
+$lang['back_to_article']   = '문서로 돌아가기';
+$lang['userpage']          = '사용자 문서';
+
+/* accessibility headlines */
+$lang['user_tools']        = '사용자 도구';
+$lang['site_tools']        = '사이트 도구';
+$lang['page_tools']        = '문서 도구';
+$lang['skip_to_content']   = '내용으로 건너뛰기';
+?>
---
+++ lang/ko/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Korean language file for config
+ *
+ */
+
+$lang['toc_place']= '어디에 목차를 넣고 싶습니까?';
+$lang['sidebar_head'] = '위 사이드바 내용 앞에 "tpl_sidebar_head.php" 포함';
+$lang['sidebar_foot'] = '위 사이드바 내용 뒤에 Include "tpl_sidebar_foot.php" 포함';
+?>
